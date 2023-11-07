9a6fed3bead2c8abc275d87338624a145ed4bbfa
Yakhya Mashrapov <yakhyokhon_m@samuraidev.uz>
2023-11-07 17:28:51 +0500

    password validation implemented

--- lib/presentation/password_editor/password_editor_screen.dart
+++ lib/presentation/password_editor/password_editor_screen.dart
@@ -1,20 +1,22 @@
 // Flutter imports:
+import 'package:blinq/app/locator.dart';
+import 'package:blinq/core/drawables/app_drawables.dart';
+import 'package:blinq/domain/repositories/profile_repository.dart';
+import 'package:blinq/utils/custom_widgets/app_bar/app_bar.dart';
+import 'package:blinq/utils/custom_widgets/app_btn.dart';
+import 'package:blinq/utils/custom_widgets/custom_textfield.dart';
+import 'package:blinq/utils/custom_widgets/keyboard_escape.dart';
 import 'package:blinq/utils/generic_bloc_state.dart';
-import 'package:flutter/material.dart';
+import 'package:blinq/utils/validator.dart';
 
 // Package imports:
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-// Project imports:
-import 'package:blinq/utils/custom_widgets/text_fields/password_text_field.dart';
-import 'package:blinq/utils/custom_widgets/app_bar/app_bar.dart';
-import 'package:blinq/utils/custom_widgets/app_btn.dart';
-import 'package:blinq/domain/repositories/profile_repository.dart';
-import 'package:blinq/utils/custom_widgets/keyboard_escape.dart';
 import 'bloc/password_editor_bloc.dart';
 import 'bloc/password_editor_event.dart';
-import 'package:blinq/app/locator.dart';
 
 class PasswordEditorScreen extends StatelessWidget {
   //
@@ -37,30 +39,71 @@ class PasswordEditorScreen extends StatelessWidget {
             bloc: bloc,
             builder: (context, state) {
               return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   const SizedBox(height: 48),
-                  PasswordTextField(
-                    isPasswordVisible: true,
-                    hintText: 'strPasswordHint'.tr(),
-                    controller: bloc.oldPasswordController,
+                  TextFieldRoundedWidget(
+                    hint: 'strPasswordHint'.tr(),
+                    textController: bloc.oldPasswordController,
+                    // validate: (value) {
+                    //   if (value.toString().isEmpty) {
+                    //     return 'strPasswordHint'.tr();
+                    //   } else if (Validator()
+                    //       .validateStrongPassword(value.toString())) {
+                    //     return 'strStrongPassword'.tr();
+                    //   }
+                    //   return null;
+                    // },
                     labelText: 'strEnterTheCurrentPassword'.tr(),
                   ),
                   const SizedBox(height: 48),
-                  PasswordTextField(
-                    hintText: 'strPasswordHint'.tr(),
-                    labelText: 'strEnterNewPassword'.tr(),
-                    isPasswordVisible: state.isCodeVisible,
-                    controller: bloc.newPasswordController,
-                    onVisibilityChanged: () =>
-                        bloc..add(OnNewPasswordVisibilityChanged()),
+                  Text(
+                    'strEnterNewPassword'.tr(),
+                    style: Theme.of(context).textTheme.bodyLarge,
+                  ),
+                  const SizedBox(height: 18,),
+                  TextFieldRoundedWidget(
+                    hint: 'strPasswordHint'.tr(),
+                    obscureText: !state.isCodeVisible,
+                    textController: bloc.newPasswordController,
+                    validate: (value) {
+                      if (value.toString().isEmpty) {
+                        return 'strPasswordHint'.tr();
+                      } else if (Validator()
+                          .validateStrongPassword(value.toString())) {
+                        return 'strStrongPassword'.tr();
+                      }
+                      return null;
+                    },
+                    suffix: IconButton(
+                      splashRadius: 1,
+                      onPressed: () =>
+                          bloc.add(OnNewPasswordVisibilityChanged()),
+                      splashColor: Colors.transparent,
+                      icon: state.isCodeVisible
+                          ? SvgPicture.asset(AppDrawables.eyeOpened)
+                          : SvgPicture.asset(AppDrawables.eyeClosed),
+                    ),
                   ),
                   const SizedBox(height: 6),
-                  PasswordTextField(
-                    hintText: 'strConfirmPassword'.tr(),
-                    controller: bloc.confirmPasswordController,
-                    isPasswordVisible: state.isConfirmCodeVisible,
-                    onVisibilityChanged: () =>
-                        bloc..add(OnPasswordConfirmVisibilityChanged()),
+                  TextFieldRoundedWidget(
+                    hint: 'strConfirmPassword'.tr(),
+                    textController: bloc.confirmPasswordController,
+                    obscureText: !state.isConfirmCodeVisible,
+                    validate: (value) {
+                      return Validator.validateConfirmPasswordMatch(
+                          value: value,
+                          password: bloc.newPasswordController.text);
+                    },
+                    suffix: IconButton(
+                      splashRadius: 1,
+                      onPressed: () =>
+                          bloc.add(OnPasswordConfirmVisibilityChanged()),
+                      splashColor: Colors.transparent,
+                      icon: state.isConfirmCodeVisible
+                          ? SvgPicture.asset(AppDrawables.eyeOpened)
+                          : SvgPicture.asset(AppDrawables.eyeClosed),
+                    ),
                   ),
                   const SizedBox(height: 48),
                   AppButton(
