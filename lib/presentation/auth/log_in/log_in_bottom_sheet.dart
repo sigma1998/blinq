28e4215537065a6578dd314ac50d81673cb8dc84
caypoh23 <caypoh23@gmail.com>
2023-12-08 13:42:21 +0500

    fix: login bottom sheet border radius

--- lib/presentation/auth/log_in/log_in_bottom_sheet.dart
+++ lib/presentation/auth/log_in/log_in_bottom_sheet.dart
@@ -1,24 +1,29 @@
+// Dart imports:
 import 'dart:io';
 import 'dart:ui';
 
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Package imports:
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:animate_do/animate_do.dart';
-import 'package:blinq/app/locator.dart';
-import 'package:blinq/core/drawables/app_drawables.dart';
-import 'package:blinq/domain/repositories/auth_repository.dart';
-import 'package:blinq/utils/custom_widgets/app_btn.dart';
+
+// Project imports:
 import 'package:blinq/utils/custom_widgets/custom_textfield.dart';
 import 'package:blinq/utils/custom_widgets/keyboard_escape.dart';
+import 'package:blinq/domain/repositories/auth_repository.dart';
+import 'package:blinq/utils/custom_widgets/app_btn.dart';
+import 'package:blinq/core/drawables/app_drawables.dart';
 import 'package:blinq/utils/generic_bloc_state.dart';
 import 'package:blinq/utils/navigation_service.dart';
 import 'package:blinq/utils/validator.dart';
-import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
-import 'bloc/login_bottom_sheet_bloc.dart';
 import 'bloc/login_bottom_sheet_event.dart';
 import 'bloc/login_bottom_sheet_state.dart';
+import 'bloc/login_bottom_sheet_bloc.dart';
+import 'package:blinq/app/locator.dart';
 
 class LogInBottomSheet extends StatefulWidget {
   const LogInBottomSheet({Key? key}) : super(key: key);
@@ -44,19 +49,27 @@ class _LogInBottomSheetState extends State<LogInBottomSheet> {
         return KeyboardEscape(
           child: FadeInUp(
             child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
               child: BackdropFilter(
                 filter: ImageFilter.blur(sigmaX: 80, sigmaY: 80),
                 child: Container(
                   padding: const EdgeInsets.all(16),
                   margin: EdgeInsets.only(
-                      bottom: MediaQuery.of(context).viewInsets.bottom),
+                    bottom: MediaQuery.of(context).viewInsets.bottom,
+                  ),
                   decoration: BoxDecoration(
-                      border: Border.all(
-                          color: Theme.of(context).colorScheme.secondary,
-                          width: 1),
-                      borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(30),
-                          topRight: Radius.circular(30))),
+                    border: Border.all(
+                      width: 1,
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
                   child: Column(
                     mainAxisSize: MainAxisSize.min,
                     crossAxisAlignment: CrossAxisAlignment.center,
@@ -64,11 +77,11 @@ class _LogInBottomSheetState extends State<LogInBottomSheet> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          const SizedBox(
-                            width: 30,
+                          const SizedBox(width: 30),
+                          Text(
+                            'strLogin'.tr(),
+                            style: Theme.of(context).textTheme.titleMedium,
                           ),
-                          Text('strLogin'.tr(),
-                              style: Theme.of(context).textTheme.titleMedium),
                           InkWell(
                             onTap: () => NavigationService.back(),
                             child: Icon(
@@ -93,10 +106,7 @@ class _LogInBottomSheetState extends State<LogInBottomSheet> {
                                 height: 40,
                               ),
                             ),
-                          if (Platform.isIOS)
-                            const SizedBox(
-                              width: 30,
-                            ),
+                          if (Platform.isIOS) const SizedBox(width: 30),
                           InkWell(
                             onTap: () {},
                             child: Image.asset(
@@ -106,34 +116,24 @@ class _LogInBottomSheetState extends State<LogInBottomSheet> {
                           )
                         ],
                       ),
-                      const SizedBox(
-                        height: 30,
-                      ),
+                      const SizedBox(height: 30),
                       Row(
                         children: [
                           _dividerLine(),
-                          const SizedBox(
-                            width: 16,
-                          ),
+                          const SizedBox(width: 16),
                           Text('strOr'.tr()),
-                          const SizedBox(
-                            width: 16,
-                          ),
+                          const SizedBox(width: 16),
                           _dividerLine(),
                         ],
                       ),
-                      const SizedBox(
-                        height: 30,
-                      ),
+                      const SizedBox(height: 30),
                       TextFieldRoundedWidget(
                         hint: 'strYourEmail'.tr(),
                         inputType: TextInputType.emailAddress,
                         textController: bloc.mailController,
                         validate: (value) => Validator.validateEmail(value),
                       ),
-                      const SizedBox(
-                        height: 30,
-                      ),
+                      const SizedBox(height: 30),
                       TextFieldRoundedWidget(
                         obscureText: !state.isCodeVisible,
                         textController: bloc.passwordController,
@@ -146,9 +146,7 @@ class _LogInBottomSheetState extends State<LogInBottomSheet> {
                         ),
                         hint: 'strYourPassword'.tr(),
                       ),
-                      const SizedBox(
-                        height: 30,
-                      ),
+                      const SizedBox(height: 30),
                       AppButton(
                         onTap: () => bloc.add(OnSubmitted()),
                         text: 'strLogin'.tr(),
@@ -156,11 +154,9 @@ class _LogInBottomSheetState extends State<LogInBottomSheet> {
                         btnColor: Colors.white,
                         txtColor: Colors.black,
                       ),
-                      const SizedBox(
-                        height: 30,
-                      ),
+                      const SizedBox(height: 30),
                       InkWell(
-                        onTap: ()=>bloc.add(OnForgotPasswordPressed()),
+                        onTap: () => bloc.add(OnForgotPasswordPressed()),
                         child: Text('strForgotpasswordOrEmail'.tr()),
                       ),
                     ],
@@ -175,6 +171,11 @@ class _LogInBottomSheetState extends State<LogInBottomSheet> {
   }
 
   Widget _dividerLine() {
-    return Expanded(child: Container(height: 2, color: Colors.white));
+    return Expanded(
+      child: Container(
+        height: 2,
+        color: Colors.white,
+      ),
+    );
   }
 }
