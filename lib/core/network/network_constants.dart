63fad095a6bc4dab3213fd081d761ba39fa07e54
caypoh23 <caypoh23@gmail.com>
2023-12-14 17:27:29 +0500

    fix: language, change password endpoints fixed, added /v1/api/

--- lib/core/network/network_constants.dart
+++ lib/core/network/network_constants.dart
@@ -39,8 +39,8 @@ class NetworkConstants {
 
   static String changeEmail = '${profile}change/email/';
   static String verifyEmail = '${profile}verify/changed/email/';
-  static String changePassword = 'change/password/in/profile/';
-  static String changeLanguage = 'change/language/';
+  static String changePassword = '/v1/api/change/password/in/profile/';
+  static String changeLanguage = '/v1/api/change/language/';
 
   static String insurance = '${profile}insurance/update/';
   static String policyHolder = '${profile}policy/holder/update/';
