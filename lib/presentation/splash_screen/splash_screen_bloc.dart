892ba9f0d1acc93b3eda505e69f0aafb10b1b170
Yakhya Mashrapov <yakhyokhon_m@samuraidev.uz>
2023-12-22 11:04:57 +0500

    notification set up

--- lib/presentation/splash_screen/splash_screen_bloc.dart
+++ lib/presentation/splash_screen/splash_screen_bloc.dart
@@ -5,6 +5,7 @@ import 'package:blinq/presentation/auth/sign_in_screen/sign_in_screen.dart';
 import 'package:blinq/presentation/intro/first_intro_screen/first_intro_screen.dart';
 import 'package:blinq/presentation/main_screen/main_screen.dart';
 import 'package:blinq/utils/navigation_service.dart';
+import 'package:blinq/utils/services/notification/notification_service.dart';
 
 class SplashScreenBloc {
   final AuthRepository authRepository;
@@ -13,6 +14,13 @@ class SplashScreenBloc {
 
   void checkStatus() async {
     final UserStatus status = authRepository.getUserStatus();
+
+
+    NotificationService.setupNotificationService();
+    final token = await NotificationService.getFcmToken();
+    print('fsjknfmldskfmlkdsmflkdsmflkdmflkdsmflkdmflkdmfldkmflsdkmflsdkmfslkd');
+    print(token);
+    print('fsjknfmldskfmlkdsmflkdsmflkdmflkdsmflkdmflkdmfldkmflsdkmflsdkmfslkd');
 
     Future.delayed(const Duration(seconds: 3)).then((_) async {
       switch (status) {
---
+++ lib/utils/services/notification/notification_service.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class NotificationService {
+  NotificationService() {
+    setupNotificationService();
+  }
+
+  static final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
+  static Future<String?> getFcmToken() async {
+    return await _firebaseMessaging.getToken();
+  }
+
+  static void setupNotificationService() async {
+    // iOS notifications setup
+    await _firebaseMessaging.requestPermission();
+    await _firebaseMessaging.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    // Init firebase notification listeners
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      _handleMessage(message);
+    });
+    FirebaseMessaging.onMessageOpenedApp.listen((event) {
+      _onAppOpened(event);
+    });
+    _firebaseMessaging
+        .getInitialMessage()
+        .then((RemoteMessage? remoteMessage) {
+
+      print('fkjdnfdskjfndkjfndksjfndksjnfkdsjnfkjsdnfkjsdnfkdsjnf');
+      print(remoteMessage);
+      print('fkjdnfdskjfndkjfndksjfndksjnfkdsjnfkjsdnfkjsdnfkdsjnf');
+    });
+    FirebaseMessaging.onBackgroundMessage(myBackgroundMessageHandler);
+  }
+
+  static Future<void> _onAppOpened(RemoteMessage message) async {
+    final data = message.data;
+  }
+
+  static _handleMessage(RemoteMessage message) async {
+    final data = message.data;
+
+    print('fkjdnfdskjfndkjfndksjfndksjnfkdsjnfkjsdnfkjsdnfkdsjnf');
+    print(data);
+    print('fkjdnfdskjfndkjfndksjfndksjnfkdsjnfkjsdnfkjsdnfkdsjnf');
+  }
+}
+
+Future<dynamic> myBackgroundMessageHandler(RemoteMessage message) async {}
